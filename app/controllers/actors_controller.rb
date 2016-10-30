cf0b64e0a5c67a1422116769079c663f4552d0eb
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-10-30 16:40:57 -0500

    Terminado

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,8 +7,19 @@ class ActorsController < ApplicationController
 		@actor = Actor.new
 	end
 
+	def create
+		@actor = Actor.new(actor_params)
+	    if @actor.save
+	      flash[:notice] = 'The actor have been saved'
+	      redirect_to actors_path
+	    else
+	      flash[:alert] = 'There was an error , try again'
+	      render :new
+	    end
+	end
+
 	private
   	def actor_params
-      params.require(:actor).permit(:name, :url, :description, :image)
+      params.require(:actor).permit(:name, :bio, :birth_day, :birth_place, :image_url, :alive, :death_date, :death_place)
     end
 end
