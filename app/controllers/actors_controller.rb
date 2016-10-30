5d3530b741dcd623b39a31cd6e526a1b87c7577a
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-10-30 16:30:39 -0500

    Reto solucionado

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,13 @@ class ActorsController < ApplicationController
 	def index
 		@actors = Actor.all
 	end
+
+	def new
+		@actor = Actor.new
+	end
+
+	private
+  	def actor_params
+      params.require(:actor).permit(:name, :url, :description, :image)
+    end
 end
